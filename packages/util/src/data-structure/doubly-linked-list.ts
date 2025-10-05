991350a14528cf8a66671fba86f84601fcbdfd2c
shawicx <d35f3153@proton.me>
2025-10-05 21:02:09 +0800

    refactor: 重构链表和双向链表实现
    - 将基于数组的实现改为真正的指针结构
    - 添加头尾指针管理，提高插入删除性能
    - 新增 indexOf、contains、isEmpty、toArray 等实用方法
    - 优化双向链表的节点遍历算法（从最近端开始）
    - 完善错误处理和边界条件检查
    - 改进反转算法的性能和正确性

--- packages/util/src/data-structure/doubly-linked-list.ts
+++ packages/util/src/data-structure/doubly-linked-list.ts
@@ -5,10 +5,27 @@ export interface DoublyLinkedNode<T> {
 }
 
 export class DoublyLinkedList<T> {
-  private nodes: Array<DoublyLinkedNode<T>>;
+  private _head: DoublyLinkedNode<T> | null = null;
+  private _tail: DoublyLinkedNode<T> | null = null;
+  private _size: number = 0;
 
   constructor() {
-    this.nodes = [];
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
+  }
+
+  /**
+   * @description 获取链表所有值的数组
+   */
+  get values(): T[] {
+    const values: T[] = [];
+    let current = this._head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
   }
 
   /**
@@ -16,7 +33,7 @@ export class DoublyLinkedList<T> {
    * @description 获取链表长度
    */
   get size(): number {
-    return this.nodes.length;
+    return this._size;
   }
 
   /**
@@ -24,7 +41,7 @@ export class DoublyLinkedList<T> {
    * @description 获取链表头部节点
    */
   get head(): DoublyLinkedNode<T> | null {
-    return this.size ? this.nodes[0] : null;
+    return this._head;
   }
 
   /**
@@ -32,85 +49,297 @@ export class DoublyLinkedList<T> {
    * @description 获取链表尾部节点
    */
   get tail(): DoublyLinkedNode<T> | null {
-    return this.size ? this.nodes[this.size - 1] : null;
+    return this._tail;
   }
 
   /**
-   * @description 在链表尾部添加节点
-   * @param index 需要插入的节点位置
-   * @param value 需要插入的节点
+   * @description 在指定位置插入节点
+   * @param index 需要插入的节点位置 (0-based)
+   * @param value 需要插入的节点值
    */
   insertAt(index: number, value: T): void {
-    const previousNode = this.nodes[index - 1] || null;
-    const nextNode = this.nodes[index] || null;
-    const node: DoublyLinkedNode<T> = { value, next: nextNode, previous: previousNode };
+    if (index < 0 || index > this._size) {
+      throw new Error(`Index ${index} is out of bounds. Size is ${this._size}`);
+    }
 
-    if (previousNode) previousNode.next = node;
-    if (nextNode) nextNode.previous = node;
-    this.nodes.splice(index, 0, node);
+    if (index === 0) {
+      this.insertFirst(value);
+      return;
+    }
+
+    if (index === this._size) {
+      this.insertLast(value);
+      return;
+    }
+
+    const newNode: DoublyLinkedNode<T> = { value, next: null, previous: null };
+    const nextNode = this._getNodeAt(index);
+    const prevNode = nextNode?.previous || null;
+
+    newNode.next = nextNode;
+    newNode.previous = prevNode;
+
+    if (prevNode) {
+      prevNode.next = newNode;
+    }
+    if (nextNode) {
+      nextNode.previous = newNode;
+    }
+
+    this._size++;
   }
 
   /**
    * @description 在链表头部添加节点
-   * @param value 需要插入的节点
+   * @param value 需要插入的节点值
    */
   insertFirst(value: T): void {
-    this.insertAt(0, value);
+    const newNode: DoublyLinkedNode<T> = { value, next: this._head, previous: null };
+
+    if (this._head) {
+      this._head.previous = newNode;
+    } else {
+      // 链表为空时，新节点同时是头和尾
+      this._tail = newNode;
+    }
+
+    this._head = newNode;
+    this._size++;
   }
 
   /**
    * @description 在链表尾部添加节点
-   * @param value 需要插入的节点
+   * @param value 需要插入的节点值
    */
   insertLast(value: T): void {
-    this.insertAt(this.size, value);
+    if (this._size === 0) {
+      this.insertFirst(value);
+      return;
+    }
+
+    const newNode: DoublyLinkedNode<T> = { value, next: null, previous: this._tail };
+
+    if (this._tail) {
+      this._tail.next = newNode;
+    }
+
+    this._tail = newNode;
+    this._size++;
   }
 
   /**
    * @description 获取节点
-   * @param index 需要获取的节点位置
+   * @param index 需要获取的节点位置 (0-based)
    */
-  getAt(index: number): DoublyLinkedNode<T> | undefined {
-    return this.nodes[index];
+  getAt(index: number): DoublyLinkedNode<T> | null {
+    return this._getNodeAt(index);
   }
 
   /**
    * @description 移除节点
-   * @param index 需要移除的节点的位置
+   * @param index 需要移除的节点的位置 (0-based)
    */
-  removeAt(index: number): Array<DoublyLinkedNode<T>> {
-    const previousNode = this.nodes[index - 1] || null;
-    const nextNode = this.nodes[index + 1] || null;
+  removeAt(index: number): DoublyLinkedNode<T> | null {
+    if (index < 0 || index >= this._size) {
+      return null;
+    }
 
-    if (previousNode) previousNode.next = nextNode;
-    if (nextNode) nextNode.previous = previousNode;
+    if (index === 0) {
+      return this.removeFirst();
+    }
 
-    return this.nodes.splice(index, 1);
+    if (index === this._size - 1) {
+      return this.removeLast();
+    }
+
+    const nodeToRemove = this._getNodeAt(index);
+    if (!nodeToRemove) {
+      return null;
+    }
+
+    const prevNode = nodeToRemove.previous;
+    const nextNode = nodeToRemove.next;
+
+    if (prevNode) {
+      prevNode.next = nextNode;
+    }
+    if (nextNode) {
+      nextNode.previous = prevNode;
+    }
+
+    this._size--;
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 删除头节点
+   */
+  removeFirst(): DoublyLinkedNode<T> | null {
+    if (!this._head) {
+      return null;
+    }
+
+    const nodeToRemove = this._head;
+    this._head = this._head.next;
+
+    if (this._head) {
+      this._head.previous = null;
+    } else {
+      // 链表变空时，tail 也要置空
+      this._tail = null;
+    }
+
+    this._size--;
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 删除尾节点
+   */
+  removeLast(): DoublyLinkedNode<T> | null {
+    if (!this._tail) {
+      return null;
+    }
+
+    const nodeToRemove = this._tail;
+    this._tail = this._tail.previous;
+
+    if (this._tail) {
+      this._tail.next = null;
+    } else {
+      // 链表变空时，head 也要置空
+      this._head = null;
+    }
+
+    this._size--;
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 查找值在链表中的索引
+   * @param value 要查找的值
+   */
+  indexOf(value: T): number {
+    let current = this._head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  /**
+   * @description 检查链表是否包含指定值
+   * @param value 要检查的值
+   */
+  contains(value: T): boolean {
+    return this.indexOf(value) !== -1;
   }
 
   /**
    * @description 清空链表
    */
   clear(): void {
-    this.nodes = [];
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
   }
 
   /**
    * @description 反转链表
    */
   reverse(): void {
-    this.nodes = this.nodes.reduce((acc: Array<DoublyLinkedNode<T>>, { value }) => {
-      const nextNode = acc[0] || null;
-      const node: DoublyLinkedNode<T> = { value, next: nextNode, previous: null };
-      if (nextNode) nextNode.previous = node;
-      return [node, ...acc];
-    }, []);
+    if (this._size <= 1) {
+      return;
+    }
+
+    let current = this._head;
+    let temp: DoublyLinkedNode<T> | null = null;
+
+    // 交换每个节点的 next 和 previous 指针
+    while (current) {
+      temp = current.previous;
+      current.previous = current.next;
+      current.next = temp;
+      current = current.previous; // 移动到下一个节点（原来的next）
+    }
+
+    // 交换头尾指针：temp 现在指向原来的头节点的 previous（即null）
+    // current 现在是 null，temp.previous 是新的头节点
+    if (temp) {
+      this._tail = this._head;
+      this._head = temp.previous;
+    }
+  }
+
+  /**
+   * @description 检查链表是否为空
+   */
+  isEmpty(): boolean {
+    return this._size === 0;
+  }
+
+  /**
+   * @description 将链表转换为数组
+   */
+  toArray(): T[] {
+    return this.values;
+  }
+
+  /**
+   * @description 从尾部开始遍历链表
+   */
+  *reverseIterator(): IterableIterator<DoublyLinkedNode<T>> {
+    let current = this._tail;
+    while (current) {
+      yield current;
+      current = current.previous;
+    }
+  }
+
+  /**
+   * @description 私有方法：获取指定位置的节点（优化版本，从较近的一端开始）
+   * @param index 节点位置
+   */
+  private _getNodeAt(index: number): DoublyLinkedNode<T> | null {
+    if (index < 0 || index >= this._size) {
+      return null;
+    }
+
+    let current: DoublyLinkedNode<T> | null;
+
+    // 从较近的一端开始遍历，提高效率
+    if (index < this._size / 2) {
+      // 从头开始
+      current = this._head;
+      for (let i = 0; i < index && current; i++) {
+        current = current.next;
+      }
+    } else {
+      // 从尾开始
+      current = this._tail;
+      for (let i = this._size - 1; i > index && current; i--) {
+        current = current.previous;
+      }
+    }
+
+    return current;
   }
 
   /**
    * @description 遍历链表
    */
   *[Symbol.iterator](): IterableIterator<DoublyLinkedNode<T>> {
-    yield* this.nodes;
+    let current = this._head;
+    while (current) {
+      yield current;
+      current = current.next;
+    }
   }
 }
--- packages/util/src/data-structure/linked-list.ts
+++ packages/util/src/data-structure/linked-list.ts
@@ -4,14 +4,27 @@ export interface LinkedNode<T> {
 }
 
 export class LinkedList<T> {
-  private nodes: Array<LinkedNode<T>>;
+  private _head: LinkedNode<T> | null = null;
+  private _tail: LinkedNode<T> | null = null;
+  private _size: number = 0;
 
   constructor() {
-    this.nodes = [];
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
   }
 
+  /**
+   * @description 获取链表所有值的数组
+   */
   get values(): T[] {
-    return this.nodes.map((node) => node.value);
+    const values: T[] = [];
+    let current = this._head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
   }
 
   /**
@@ -19,7 +32,7 @@ export class LinkedList<T> {
    * @description 链表长度
    */
   get size(): number {
-    return this.nodes.length;
+    return this._size;
   }
 
   /**
@@ -27,7 +40,7 @@ export class LinkedList<T> {
    * @description 链表中第一个节点
    */
   get head(): LinkedNode<T> | null {
-    return this.size ? this.nodes[0] : null;
+    return this._head;
   }
 
   /**
@@ -35,21 +48,37 @@ export class LinkedList<T> {
    * @description 链表中最后一个节点
    */
   get tail(): LinkedNode<T> | null {
-    return this.size ? this.nodes[this.size - 1] : null;
+    return this._tail;
   }
 
   /**
    * @description 在指定位置插入节点
-   * @param index 插入节点的位置
+   * @param index 插入节点的位置 (0-based)
    * @param value 插入节点的值
    */
   insertAt(index: number, value: T): void {
-    const previousNode = this.nodes[index - 1] || null;
-    const nextNode = this.nodes[index] || null;
-    const node: LinkedNode<T> = { value, next: nextNode };
+    if (index < 0 || index > this._size) {
+      throw new Error(`Index ${index} is out of bounds. Size is ${this._size}`);
+    }
 
-    if (previousNode) previousNode.next = node;
-    this.nodes.splice(index, 0, node);
+    if (index === 0) {
+      this.insertFirst(value);
+      return;
+    }
+
+    if (index === this._size) {
+      this.insertLast(value);
+      return;
+    }
+
+    const newNode: LinkedNode<T> = { value, next: null };
+    const prevNode = this._getNodeAt(index - 1);
+
+    if (prevNode) {
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+      this._size++;
+    }
   }
 
   /**
@@ -57,7 +86,14 @@ export class LinkedList<T> {
    * @param value 插入节点的值
    */
   insertFirst(value: T): void {
-    this.insertAt(0, value);
+    const newNode: LinkedNode<T> = { value, next: this._head };
+    this._head = newNode;
+
+    if (this._size === 0) {
+      this._tail = newNode;
+    }
+
+    this._size++;
   }
 
   /**
@@ -65,51 +101,200 @@ export class LinkedList<T> {
    * @param value 插入节点的值
    */
   insertLast(value: T): void {
-    this.insertAt(this.size, value);
+    if (this._size === 0) {
+      this.insertFirst(value);
+      return;
+    }
+
+    const newNode: LinkedNode<T> = { value, next: null };
+
+    if (this._tail) {
+      this._tail.next = newNode;
+    }
+
+    this._tail = newNode;
+    this._size++;
   }
 
   /**
    * @description 获取指定位置的节点
-   * @param index 节点位置
+   * @param index 节点位置 (0-based)
    */
-  getAt(index: number): LinkedNode<T> | undefined {
-    return this.nodes[index];
+  getAt(index: number): LinkedNode<T> | null {
+    return this._getNodeAt(index);
   }
 
   /**
    * @description 删除指定位置的节点
-   * @param index 节点位置
+   * @param index 节点位置 (0-based)
    */
-  removeAt(index: number): Array<LinkedNode<T>> {
-    const previousNode = this.nodes[index - 1];
-    const nextNode = this.nodes[index + 1] || null;
+  removeAt(index: number): LinkedNode<T> | null {
+    if (index < 0 || index >= this._size) {
+      return null;
+    }
 
-    if (previousNode) previousNode.next = nextNode;
+    if (index === 0) {
+      return this.removeFirst();
+    }
 
-    return this.nodes.splice(index, 1);
+    const prevNode = this._getNodeAt(index - 1);
+    if (!prevNode || !prevNode.next) {
+      return null;
+    }
+
+    const nodeToRemove = prevNode.next;
+    prevNode.next = nodeToRemove.next;
+
+    // 如果删除的是尾节点，更新 tail 指针
+    if (nodeToRemove === this._tail) {
+      this._tail = prevNode;
+    }
+
+    this._size--;
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 删除头节点
+   */
+  removeFirst(): LinkedNode<T> | null {
+    if (!this._head) {
+      return null;
+    }
+
+    const nodeToRemove = this._head;
+    this._head = this._head.next;
+
+    if (this._size === 1) {
+      this._tail = null;
+    }
+
+    this._size--;
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 删除尾节点
+   */
+  removeLast(): LinkedNode<T> | null {
+    if (this._size === 0) {
+      return null;
+    }
+
+    if (this._size === 1) {
+      return this.removeFirst();
+    }
+
+    const prevNode = this._getNodeAt(this._size - 2);
+    if (!prevNode) {
+      return null;
+    }
+
+    const nodeToRemove = this._tail;
+    prevNode.next = null;
+    this._tail = prevNode;
+    this._size--;
+
+    return nodeToRemove;
+  }
+
+  /**
+   * @description 查找值在链表中的索引
+   * @param value 要查找的值
+   */
+  indexOf(value: T): number {
+    let current = this._head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  /**
+   * @description 检查链表是否包含指定值
+   * @param value 要检查的值
+   */
+  contains(value: T): boolean {
+    return this.indexOf(value) !== -1;
   }
 
   /**
    * @description 清空链表
    */
   clear(): void {
-    this.nodes = [];
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
   }
 
   /**
    * @description 反转链表
    */
   reverse(): void {
-    this.nodes = this.nodes.reduce(
-      (acc: Array<LinkedNode<T>>, { value }) => [{ value, next: acc[0] || null }, ...acc],
-      [],
-    );
+    if (this._size <= 1) {
+      return;
+    }
+
+    let prev: LinkedNode<T> | null = null;
+    let current = this._head;
+    this._tail = this._head; // 原来的头变成新的尾
+
+    while (current) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this._head = prev; // 最后的prev就是新的头
+  }
+
+  /**
+   * @description 检查链表是否为空
+   */
+  isEmpty(): boolean {
+    return this._size === 0;
+  }
+
+  /**
+   * @description 将链表转换为数组
+   */
+  toArray(): T[] {
+    return this.values;
+  }
+
+  /**
+   * @description 私有方法：获取指定位置的节点
+   * @param index 节点位置
+   */
+  private _getNodeAt(index: number): LinkedNode<T> | null {
+    if (index < 0 || index >= this._size) {
+      return null;
+    }
+
+    let current = this._head;
+    for (let i = 0; i < index && current; i++) {
+      current = current.next;
+    }
+
+    return current;
   }
 
   /**
    * @description 遍历链表
    */
   *[Symbol.iterator](): IterableIterator<LinkedNode<T>> {
-    yield* this.nodes;
+    let current = this._head;
+    while (current) {
+      yield current;
+      current = current.next;
+    }
   }
 }
